34543f49d8507197812bdeae7ca94e850a8235f0
Alex Manzo <manzoalex17@gmail.com>
2019-11-07 11:28:54 -0500

    Update JS code to use Drupal behaviors and JS classes.

--- storybook/components/atoms/button/button.js
+++ storybook/components/atoms/button/button.js
@@ -1,25 +1,48 @@
-/*
-  Example JavaScript for Storybook/Drupal projects.
-*/
+// /*
+//   Example JavaScript for Storybook/Drupal projects.
+// */
 
-export const onMouseover = e => {
+// Create your Class function.
+export class ButtonScript {
+  // The btn is passed by Drupal in below code.
+  // It's passed by Storybook withn the useEffect block.
+  constructor(btn) {
+    Object.defineProperty(this, 'button', { value: btn || null });
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    this.button.addEventListener('mouseover', this.onClick);
+  }
+
+  onClick() {
+    console.log(this);
+  }
+}
+
+export default ButtonScript;
+
+// Wrap Drupal specific code in a try/catch.
+try {
+  // Basically, if Storybook, throw an error and stop executing code.
+  if (!Drupal.behaviors) {
+    throw new Error("You're not in Drupal");
+  }
+  // Wrap in Drupal behaviors.
   // eslint-disable-next-line
-  console.log(e.currentTarget);
-};
-
-export const Button = () => {
-  const buttons = document.querySelectorAll('.button');
-  buttons.forEach(button => {
-    button.addEventListener('mouseover', onMouseover);
-  });
-};
-
-/*
-  Only call if item is on the page.
-  Do not call if Storybook wrapper is present.
-  Function will be called separately in SB story.
-*/
-
-if (document.querySelectorAll('.button').length > 0) {
-  Button();
+  (({ behaviors }, { theme_name }) => {
+    behaviors.button = {
+      attach(context) {
+        context.querySelectorAll('.button').forEach(button => {
+          // eslint-disable-next-line
+          new ButtonScript(button);
+        });
+      },
+    };
+  })(Drupal, drupalSettings);
+} catch (err) {
+  // Catch the error we threw above.
+  if (err.message === "You're not in Drupal") {
+    console.log('Drupal specific code ignored in Storybook.');
+  }
 }
--- storybook/components/atoms/button/button.stories.js
+++ storybook/components/atoms/button/button.stories.js
@@ -4,11 +4,14 @@ import '../../../styles/index.scss';
 
 import Buttons from './button.twig';
 import { button } from './button.data';
-import { Button } from './button';
+import { ButtonScript } from './button';
 
 storiesOf('Atoms/Buttons', module).add('Buttons', () => {
   useEffect(() => {
-    Button();
+    document.querySelectorAll('.button').forEach(btn => {
+      // eslint-disable-next-line
+      new ButtonScript(btn);
+    });
   }, []);
   return Buttons(button);
 });
