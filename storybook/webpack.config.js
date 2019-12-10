1d50d479150d9b6208ac4071ea8533869d4baea5
Alex Manzo <manzoalex17@gmail.com>
2019-12-10 10:39:32 -0500

    Update webpack config for docker

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,39 +1,87 @@
 const path = require('path');
+const webpack = require('webpack');
 
 // Import dependencies.
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-
-// Import common configurations.
-const common = require('./webpack.common');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 
 // Webpack configuration.
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production' ? true : false;
-
   return {
     mode: isProduction ? 'production' : 'development',
     entry: {
-      scripts: './scripts/index.js',
+      scripts: path.resolve(__dirname, 'scripts/index.js'),
     },
     output: {
       filename: '[name].js',
       path: isProduction
         ? path.resolve(__dirname, '../build')
-        : path.resolve(__dirname, '../dev-build'),
+        : path.resolve(__dirname, '../build'),
     },
     resolve: { extensions: ['.js'] },
     module: {
       rules: [
-        common.javascript,
-        // Extend common CSS configuration with those used in build process.
+        // JavaScript
         {
-          ...common.css,
-          use: isProduction
-            ? [MiniCssExtractPlugin.loader, ...common.css.use]
-            : ['style-loader', ...common.css.use],
+          test: /\.(js)$/,
+          exclude: '/node_modules/',
+          use: [
+            'cache-loader',
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  [
+                    '@babel/env',
+                    {
+                      targets: { chrome: '63', ie: '11' },
+                    },
+                  ],
+                ],
+                plugins: [
+                  [
+                    '@babel/plugin-proposal-decorators',
+                    { decoratorsBeforeExport: true },
+                  ],
+                  ['@babel/proposal-class-properties', { loose: true }],
+                  '@babel/proposal-object-rest-spread',
+                ],
+              },
+            },
+            'eslint-loader',
+          ],
         },
-        common.assets,
+        // CSS
+        {
+          test: /\.(scss|css)$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'cache-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                config: { path: path.resolve(__dirname, 'postcss.config.js') },
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+        // Assets
+        {
+          test: /\.(woff|woff2|eot|ttf|svg|ico|jpe?g|png)$/,
+          use: ['cache-loader', 'file-loader'],
+        },
       ],
     },
     /**
@@ -49,7 +97,29 @@ module.exports = (env, argv) => {
         filename: 'index.css',
         path: path.resolve(__dirname, '../build'),
       }),
-      ...common.plugins,
+      new webpack.NoEmitOnErrorsPlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+      new FixStyleOnlyEntriesPlugin(),
+      new StyleLintPlugin({
+        configFile: path.resolve(__dirname, '.stylelintrc'),
+        ignorePath: path.resolve(__dirname, '.eslintignore'), // Use .eslintignore because it has ignored directories/files already.
+        syntax: 'scss',
+      }),
+      new CopyWebpackPlugin([
+        {
+          context: path.join(__dirname, 'icons'),
+          from: path.join(__dirname, 'icons/**/*'),
+          to: path.join(__dirname, '../build/icons'),
+        },
+        {
+          context: path.join(__dirname, 'fonts'),
+          from: path.join(__dirname, 'fonts/**/*'),
+          to: path.join(__dirname, '../build/fonts'),
+        },
+      ]),
       new FriendlyErrorsWebpackPlugin(),
     ],
     stats: 'errors-only',
